bdc6e2139e456b9d6345ab35760d348c59d0d4fc
Bram Kok <bram@bramkok.com>
2016-07-10 11:13:22 +0200

    config/fish: Add fish config file

---
+++ config/fish/config.fish
@@ -0,0 +1,14 @@
+set fish_greeting
+
+set -x vi nvim
+set -x vim nvim
+set -x EDITOR nvim
+set -x GREP_COLOR "1;37;45"
+set -x LC_ALL en_US.UTF-8
+set -x LANG en_US.UTF-8
+
+# Navigation
+function ..    ; cd .. ; end
+function ...   ; cd ../.. ; end
+function ....  ; cd ../../.. ; end
+function ..... ; cd ../../../.. ; end
